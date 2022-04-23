5834bbd8d8d037173d12fc86cd3eecd0bafa2034
Wanderson <lopez.wanderson@gmail.com>
2022-04-23 12:48:05 -0600

    Se crea la subclase de CoreData para la orden

---
+++ Order+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Order+CoreDataClass.swift
+//  PizzaRestaurant
+//
+//  Created by Wanderson López on 23/04/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Order)
+public class Order: NSManagedObject {
+
+}
---
+++ Order+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  Order+CoreDataProperties.swift
+//  PizzaRestaurant
+//
+//  Created by Wanderson López on 23/04/22.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Order {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Order> {
+        return NSFetchRequest<Order>(entityName: "Order")
+    }
+
+    @NSManaged public var id: UUID?
+    @NSManaged public var pizzaType: String?
+    @NSManaged public var tableNumber: String?
+    @NSManaged public var status: String?
+    @NSManaged public var numberOfSlices: Int16
+
+}
+
+extension Order : Identifiable {
+
+}

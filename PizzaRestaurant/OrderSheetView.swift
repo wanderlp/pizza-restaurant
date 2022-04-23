25b6b6751c760a82d77a5dd1f4245ca5d54f196b
Wanderson <lopez.wanderson@gmail.com>
2022-04-23 13:13:54 -0600

    Se agrega el order sheet view

---
+++ PizzaRestaurant/OrderSheetView.swift
@@ -0,0 +1,20 @@
+//
+//  OrderSheetView.swift
+//  PizzaRestaurant
+//
+//  Created by Wanderson López on 23/04/22.
+//
+
+import SwiftUI
+
+struct OrderSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderSheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderSheetView()
+    }
+}
